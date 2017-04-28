19abbcea494c79ec3a4091a4c8b33c00a2e30e61
Cody <cjpff14@gmail.com>
2017-04-27 23:22:33 -0400

    initial commit

---
+++ ArkIntel/MainWindow.xaml.cs
@@ -0,0 +1,278 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using WinSCP;
+
+namespace ArkIntel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public static event EventHandler LstDinosSelectionChanged;
+        public static event EventHandler DgDinoDataSelectionChanged;
+        public FileInfo[] Files;
+        public static DirectoryInfo Dir = new DirectoryInfo(@"output");
+        public static List<DinoStats> GlobalDinoData;
+        public static bool MapOverlayOpen;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+        public void InitializeData()
+        {
+            RunArkTools();
+            PopulateListbox();
+            CountTotalDinos();
+            lbStatus.Content = "Worldfile last saved: " + File.GetLastWriteTime("TheVolcano.ark").ToString("MM/dd HH:mm:ss");
+        }
+
+        private void lstDinos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (lstDinos.SelectedIndex == -1)
+            {
+                return;
+            }
+            string selectedValue = Files[lstDinos.SelectedIndex].Name;
+
+            GlobalDinoData = JsonConvert.DeserializeObject<List<DinoStats>>(File.ReadAllText($@"{Dir}\{selectedValue}"));
+            dgDinoData.ItemsSource = GlobalDinoData;
+            
+            lbSelectedDinoCount.Content = $"Selected count: {GlobalDinoData.Count}";
+            btnShowMap.IsEnabled = true;
+
+            LstDinosSelectionChanged?.Invoke(sender, e);
+        }
+
+        private void dgDinoData_SelectionChanged(object sender, EventArgs e)
+        {
+            DgDinoDataSelectionChanged?.Invoke(sender, e);
+        }
+
+        private async void btnRefreshData_Click(object sender, RoutedEventArgs e)
+        {
+            lbStatus.Content = "Connecting to FTP server...";
+            await Task.Run(() => DownloadNewData());
+            lbStatus.Content = "Save data downloaded. Running Ark-Tools...";
+            InitializeData();
+        }
+
+        private void btnShowMap_Click(object sender, RoutedEventArgs e)
+        {
+            if (MapOverlayOpen)
+            {
+                return;
+            }
+            MapOverlay mapOverlay = new MapOverlay
+            {
+                Left = Left + Width,
+                Top = Top
+            };
+            mapOverlay.Show();
+            MapOverlayOpen = true;
+        }
+
+        private void RunArkTools()
+        {
+            if (!Directory.Exists($@"{Dir}"))
+            {
+                Directory.CreateDirectory("output");
+            }
+
+            foreach (FileInfo file in Dir.GetFiles()) // Clear any old data
+            {
+                file.Delete();
+            }
+
+            System.Diagnostics.Process arktools = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo =
+                new System.Diagnostics.ProcessStartInfo
+                {
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                    FileName = "java",
+                    Arguments = "-jar ark-tools.jar wild TheVolcano.ark output"
+                };
+            arktools.StartInfo = startInfo;
+            arktools.Start();
+            arktools.WaitForExit();
+
+            if (File.Exists($@"{Dir}\classes.json"))
+            {
+                File.Delete($@"{Dir}\classes.json");
+            }
+        }
+
+        private int DownloadNewData()
+        {
+            if (File.Exists("TheVolcano.ark"))
+            {
+                File.Delete("TheVolcano.ark");
+            }
+            try
+            {
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Ftp,
+                    HostName = "",
+                    UserName = "",
+                    Password = ""
+                };
+
+                using (Session session = new Session())
+                {
+                    session.FileTransferProgress += SessionFileTransferProgress;
+
+                    session.Open(sessionOptions);
+
+                    try
+                    {
+                        session.GetFiles("/ShooterGame/Saved/SavedArk39950/TheVolcano.ark", "TheVolcano.ark").Check();
+                    }
+                    catch (Exception)
+                    {
+                        lbStatus.Content = "Error";
+                    }
+                }
+                return 0;
+            }
+            catch (Exception)
+            {
+                lbStatus.Content = "Error";
+                return 1;
+            }
+        }
+
+        private void SessionFileTransferProgress(object sender, FileTransferProgressEventArgs e)
+        {
+            // Print transfer progress
+            Dispatcher.Invoke(() =>
+            {
+                lbStatus.Content = $"Downloading: {e.FileName} - ({e.FileProgress:P0})";
+            });            
+        }
+
+        private void PopulateListbox()
+        {
+            lstDinos.Items.Clear();
+
+            Files = Dir.GetFiles("*.json");
+
+            foreach (var file in Files)
+            {
+                string fileCategorized;
+
+                // Remove extra filename clutter
+                var fileCleaned = file.Name.Substring(0, file.Name.IndexOf("_Character", StringComparison.Ordinal));
+
+                // Categorizing any "special" dinos
+                if (file.Name.Contains("SE_"))
+                {
+                    fileCategorized = fileCleaned.Remove(0, 3) + " (SE)";
+                }
+                else if (file.Name.Contains("Child"))
+                {
+                    fileCategorized = fileCleaned + " (Child)";
+                }
+                else if (!file.Name.Contains("Core"))
+                {
+                    fileCategorized = fileCleaned + " (Base)";
+                }
+                else
+                {
+                    fileCategorized = fileCleaned;
+                }
+
+                // Elemental dino handling
+                if (file.Name.Contains("Lightning"))
+                {
+                    fileCategorized = fileCategorized + " (L)";
+                }
+                else if (file.Name.Contains("Fire"))
+                {
+                    fileCategorized = fileCategorized + " (F)";
+                }
+                else if (file.Name.Contains("Poison"))
+                {
+                    fileCategorized = fileCategorized + " (P)";
+                }
+
+                // Add to listbox
+                lstDinos.Items.Add(fileCategorized);
+            }
+        }
+
+        public void CountTotalDinos()
+        {
+            Files = Dir.GetFiles("*.json");
+            var totalDinos = 0;
+            foreach (FileInfo file in Files)
+            {
+                var dinoData = JsonConvert.DeserializeObject<List<DinoStats>>(File.ReadAllText($@"{Dir}\{file}"));
+                totalDinos = totalDinos + dinoData.Count;
+            }
+            lbTotalDinoCount.Content = $"Total count: {totalDinos}";
+        }
+
+        public static bool IsWindowOpen<T>(string name = "") where T : Window
+        {
+            return string.IsNullOrEmpty(name)
+               ? Application.Current.Windows.OfType<T>().Any()
+               : Application.Current.Windows.OfType<T>().Any(w => w.Name.Equals(name));
+        }
+    }
+
+    public class LevelToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            int? input = value as int?;
+            if (input > 90)
+            {
+                return Brushes.LightGreen;
+            }
+            else if (input > 60)
+            {
+                return Brushes.Yellow;
+            }
+            else if (input > 30)
+            {
+                return Brushes.Orange;
+            }
+            else return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class WildLevels
+    {
+        public int health { get; set; }
+        public int stamina { get; set; }
+        public int oxygen { get; set; }
+        public int food { get; set; }
+        public int weight { get; set; }
+        public int melee { get; set; }
+        public int speed { get; set; }
+    }
+    public class DinoStats
+    {
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public int baseLevel { get; set; }
+        public bool? female { get; set; }
+        public WildLevels wildLevels { get; set; }
+    }
+}
---
+++ ArkIntel/MapOverlay.xaml.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ArkIntel
+{
+    /// <summary>
+    /// Interaction logic for MapOverlay.xaml
+    /// </summary>
+    public partial class MapOverlay
+    {
+        public MapOverlay()
+        {
+            InitializeComponent();
+            MainWindow.LstDinosSelectionChanged += MainWindow_lstDinosSelectionChanged;
+            MainWindow.DgDinoDataSelectionChanged += MainWindow_dgDinoDataSelectionChanged;
+            DrawDinoLocations();
+        }
+
+        private void DrawDinoLocations()
+        {
+            canvasDinoPositions.Children.Clear();
+
+            var dinoData = MainWindow.GlobalDinoData;
+
+            var index = dinoData.Count;
+            var i = 0;
+            while (i < index)
+            {
+                DrawShape(10, ScaleCoordsToMap(dinoData[i].lon), ScaleCoordsToMap(dinoData[i].lat), Colors.Black, ConvertLevelToColor(dinoData[i].baseLevel), ConvertLevelToZIndex(dinoData[i].baseLevel));
+                i++;
+            }
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            KeyDown += MapOverlay_KeyDown;
+        }
+    
+        private void MapOverlay_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MainWindow.MapOverlayOpen = false;
+        }
+
+        private void DrawShape(int circleSize, double x, double y, Color strokeColor, Color fillColor, int zindex)
+        {
+            var dotSize = circleSize;
+
+            var currentDot = new Ellipse
+            {
+                Stroke = new SolidColorBrush(strokeColor),
+                StrokeThickness = 2,
+                Height = dotSize,
+                Width = dotSize,
+                Fill = new SolidColorBrush(fillColor),
+                Margin = new Thickness(x, y, 0, 0)
+            };
+            // Sets the position.
+            canvasDinoPositions.Children.Add(currentDot);
+            Panel.SetZIndex(currentDot, zindex);
+
+        }
+
+        public double ScaleCoordsToMap(double coord)
+        {
+            return coord * 5.8;
+        }
+
+        public Color ConvertLevelToColor(int baseLevel)
+        {
+            int? input = baseLevel;
+            if (input > 90)
+            {
+                return Colors.LightGreen;
+            }
+            else if (input > 60)
+            {
+                return Colors.Yellow;
+            }
+            else if (input > 30)
+            {
+                return Colors.Orange;
+            }
+            else return Colors.Red;
+        }
+
+        public int ConvertLevelToZIndex(int baseLevel)
+        {
+            int? input = baseLevel;
+            if (input > 90)
+            {
+                return 4;
+            }
+            else if (input > 60)
+            {
+                return 3;
+            }
+            else if (input > 30)
+            {
+                return 2;
+            }
+            else return 1;
+        }
+
+        private void MainWindow_lstDinosSelectionChanged(object sender, EventArgs e)
+        {
+            DrawDinoLocations();
+        }
+
+        private void MainWindow_dgDinoDataSelectionChanged(object sender, EventArgs e)
+        {
+            canvasDinoPositionsHighlighted.Children.Clear();
+
+            var dinoData = MainWindow.GlobalDinoData;
+            var i = ((MainWindow)Application.Current.MainWindow).dgDinoData.SelectedIndex;
+
+            if (i == -1) return;
+
+            // Separate DrawShape logic so we can animate the the specific shape.
+            const int dotSize = 10;
+            var currentDot = new Ellipse
+            {
+                Stroke = new SolidColorBrush(Colors.Black),
+                StrokeThickness = 2,
+                Height = dotSize,
+                Width = dotSize,
+                RenderTransformOrigin = new Point(0.5, 0.5),
+                Fill = new SolidColorBrush(Colors.White),
+                Margin = new Thickness(ScaleCoordsToMap(dinoData[i].lon), ScaleCoordsToMap(dinoData[i].lat), 0, 0)
+            };
+            // Sets the position.
+            canvasDinoPositionsHighlighted.Children.Add(currentDot);
+
+            var trans = new ScaleTransform();
+            currentDot.RenderTransform = trans;
+            var animation = new DoubleAnimation
+            {
+                From = 1,
+                To = 2,
+                Duration = TimeSpan.FromSeconds(1),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+            trans.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+            trans.BeginAnimation(ScaleTransform.ScaleYProperty, animation);
+        }
+    }
+}
